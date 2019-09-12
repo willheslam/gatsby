c86d4f0dbbbf7d888ed2953a81a88c14ef1d45a6
Ellie Foreman <ellieforeman@Ellies-MacBook-Pro.local>
2019-09-12 12:08:32 +0100

    added placeholders for sponor logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,84 +1,99 @@
 /* eslint-disable no-undef, react/prop-types */
 import React from 'react';
 import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import { Box, Flex } from '../components/Layout';
 import PageWrapper from '../components/PageWrapper';
 import colors from '../utils/colors';
 import ButtonPrimary, { ButtonSecondary } from '../components/Buttons';
+import { css } from 'react-emotion';
+
+const imgStyle = css`
+  border-radius: 5px;
+`;
 
 export default ({ data }) => {
   const myData = data.allContentJson.edges[0].node.index;
+  const imageData = data.file.childImageSharp;
   return (
     <PageWrapper>
+      {/* this is the home page box with title and information */}
       <Box bg={colors.primary}>
         <Box
-          width={[1, 1, '1200px']}
+          width={[1, 1, '800px']}
           m={['3.5rem 0', '3.5rem 0', '3.5rem auto']}
           px={[3, 3, 0]}
           color={colors.secondary}
           textAlign="center"
         >
-          <h1>{myData.title}</h1>
-          <h3>{myData.subtitle}</h3>
-          <Flex justifyContent="center" wrap={['wrap', 'wrap', 'nowrap']}>
-            <Box m={[1]} width={[1, 1 / 2, 1 / 4]}>
-              <Link to="/about">
-                <ButtonPrimary>About</ButtonPrimary>
-              </Link>
-            </Box>
-            <Box m={[1]} width={[1, 1 / 2, 1 / 4]}>
-              <Link to="/about">
-                <ButtonSecondary>About</ButtonSecondary>
-              </Link>
-            </Box>
-          </Flex>
-
-          <Flex justifyContent="center" wrap={['wrap', 'wrap', 'nowrap']}>
-            <Box m={[1]} width={[1, 1 / 2, 1 / 4]}>
-              <Link to="/about">
-                <ButtonPrimary>About</ButtonPrimary>
-              </Link>
-            </Box>
-            <Box m={[1]} width={[1, 1 / 2, 1 / 4]}>
-                <image>About</image>
-            </Box>
-          </Flex>
+        <h1>{myData.title}</h1>
+        <p>{myData.aboutText}</p>
         </Box>
       </Box>
 
-      <Box bg={colors.primary} py={[3, 3, 4]}>
-        <Box
-          width={[1, 1, '1200px']}
-          m="0 auto"
-          px={[3, 3, 0]}
-          color={colors.secondary}
-        >
-        <ul>
-        <li>image 1</li>
-        <li>image 2</li>
-        <li>image 3</li>
-        <li>image 4</li>
-        <li>image 5</li>
-        </ul>
-          <h1>This could be another section</h1>
-          <p>With another text object</p>
-        </Box>
+      <Box bg={colors.primary} pb={[4]}>
+        <Flex justifyContent="center" wrap={['wrap', 'nowrap', 'nowrap']}>
+          <Box m={[3]} width={[ "90px" , "130px", "130px"]}>
+            <Img
+            className={imgStyle}
+            alt="Picture of X"
+            fluid={imageData.fluid}
+            />
+          </Box>
+          <Box m={[3]} width={[ "90px" , "130px", "130px"]}>
+            <Img
+            className={imgStyle}
+            alt="Picture of X"
+            fluid={imageData.fluid}
+            />
+          </Box>
+          <Box m={[3]} width={[ "90px" , "130px", "130px"]}>
+            <Img
+            className={imgStyle}
+            alt="Picture of X"
+            fluid={imageData.fluid}
+            />
+          </Box>
+          <Box m={[3]} width={[ "90px" , "130px", "130px"]}>
+            <Img
+            className={imgStyle}
+            alt="Picture of X"
+            fluid={imageData.fluid}
+            />
+          </Box>
+          <Box m={[3]} width={[ "90px" , "130px", "130px"]}>
+            <Img
+            className={imgStyle}
+            alt="Picture of X"
+            fluid={imageData.fluid}
+            />
+          </Box>
+        </Flex>
       </Box>
     </PageWrapper>
   );
 };
 
-export const pageQuery = graphql`
-  query contentQuery {
-    allContentJson {
-      edges {
-        node {
-          index {
-            title
-            subtitle
-          }
-        }
+export const query = graphql`
+query {
+  file(relativePath: { eq: "arts-council.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
       }
     }
   }
+  allContentJson {
+		edges {
+			node {
+				index {
+					title
+					aboutText
+				}
+      }
+    }
+  }
+}
 `;
+
+// allContentJson - will query and array where there are multiple items. Any query not beginning with 'all' with query a single object
